4c67710717defc90caae5d2711371c3f52964a5d
babyblue <babyblue@live.com>
2015-12-29 15:28:41 +0800

    注册

---
+++ test/test/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  test
+//
+//  Created by babyblue on 15/12/29.
+//  Copyright © 2015年 babyblue. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSLog(@"laksdhksjadhfkjashf");
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
